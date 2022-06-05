c651e3bccc723a97c921ab5061a693a5c421ff96
shirotamaki <shiro.tamaki63@gmail.com>
2022-06-05 22:21:13 +0900

    Add: 編集機能を実装

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const FooApp = {
   data () {
     return {
+      text: "",
+      editIndex: -1,
       todos: []
     }
   },
@@ -15,23 +17,52 @@ const FooApp = {
   mounted: function() {
     this.todos = JSON.parse(localStorage.getItem('todos')) || []
   },
+
   methods: {
-    addItem: function () {
-      this.todos.push(this.newItem)
-      this.newItem = ''
-    },
-    deleteItem: function (index) {
-      if (confirm('Are you sure?')) {
-        this.todos.splice(index, 1)
+    // addItem: function () {
+    //   this.todos.push(this.newItem)
+    //   this.newItem = ''
+    // },
+    // deleteItem: function (index) {
+    //   if (confirm('Are you sure?')) {
+    //     this.todos.splice(index, 1)
+    //   }
+    //   this.newItem = ''
+    // },
+    //
+    // 送信ボタンをクリックしたら以下を実行
+    setItems () {
+      if (this.editIndex === -1) {
+        this.todos.push(this.text)
+      } else {
+        this.todos.splice(this.editIndex, 1, this.text)
       }
-      this.newItem = ''
+      this.cancel()
     },
+    cancel() {
+      this.text = ""
+      this.editIndex = -1
+    },
+    edit (index) {
+      this.editIndex = index
+      this.text = this.todos[index]
+      this.$refs.editor.focus()
+    },
+    remove(index) {
+      this.todos.splice(index, 1)
+    }
   },
+
   computed: {
     remaining: function () {
       return this.todos.length
+    },
+    changeButtonText() {
+      return this.editIndex === -1 ? "追加" : "編集"
     }
   }
 }
 
 Vue.createApp(FooApp).mount('#app')
+
+
